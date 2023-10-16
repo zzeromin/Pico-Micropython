ea324d2af9eb046f480954ae1644f30638d4e8d5
Youngmin Seo <zzeromin@gmail.com>
2023-10-16 23:23:33 +0900

    Update ex_pwm_led.py

--- ex_pwm_led.py
+++ ex_pwm_led.py
@@ -2,10 +2,14 @@ from machine import Pin, PWM
 from time import sleep
 
 led = PWM(Pin(22))
+
 led.freq(1000)
 
 while True:
-    for duty in range(65000):
-        print(duty)
+    for duty in range(65535):
+        led.duty_u16(duty)
+        sleep(0.0001)
+        
+    for duty in range(65535, 0, -1):
         led.duty_u16(duty)
         sleep(0.0001)
